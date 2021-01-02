019ca9158a60c91ad358e2529c2915f18e0f5585
andre <andre@FENRIR>
2021-01-02 15:29:11 -0600

    -Se agrego dependencia primefaces
    -Se agrego inicializacion de variables en ProfesorBean

--- primefacesSpringDemo/src/main/java/com/softwareValelciano/primefaces/primefacesDemo/controller/ProfesorBean.java
+++ primefacesSpringDemo/src/main/java/com/softwareValelciano/primefaces/primefacesDemo/controller/ProfesorBean.java
@@ -1,10 +1,34 @@
 package com.softwareValelciano.primefaces.primefacesDemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import com.softwareValelciano.primefaces.primefacesDemo.entity.Profesor;
+import com.softwareValelciano.primefaces.primefacesDemo.service.ProfesorService;
 
 @Component
 @Scope("session")
 public class ProfesorBean {
 
+	@Autowired
+	ProfesorService profesorService;
+
+	Profesor profesorEntity;
+	List<Profesor> listaProfesores;
+    List<Profesor> listaProfesoresFiltrada ;
+    
+    @PostConstruct
+    public void init() {
+        profesorEntity = new Profesor();
+    	listaProfesores = new ArrayList<Profesor>();
+        listaProfesoresFiltrada  = new ArrayList<Profesor>();
+    }
+    
+    
 }
