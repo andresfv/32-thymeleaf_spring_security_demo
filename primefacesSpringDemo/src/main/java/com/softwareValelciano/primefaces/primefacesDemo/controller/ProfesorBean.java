ffef4196827dc8aeb8547285f66aec8ab1b13421
andre <andre@FENRIR>
2021-01-02 14:31:34 -0600

    Se creo ProfesorBean

---
+++ primefacesSpringDemo/src/main/java/com/softwareValelciano/primefaces/primefacesDemo/controller/ProfesorBean.java
@@ -0,0 +1,10 @@
+package com.softwareValelciano.primefaces.primefacesDemo.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("session")
+public class ProfesorBean {
+
+}
