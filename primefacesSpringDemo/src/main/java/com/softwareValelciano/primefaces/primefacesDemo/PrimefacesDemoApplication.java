4e14093bc62fccfb17b62ecce2b207864a3ac19d
andre <andre@FENRIR>
2021-01-02 11:42:47 -0600

    Exportación de proyecto a repositorio Git

---
+++ primefacesSpringDemo/src/main/java/com/softwareValelciano/primefaces/primefacesDemo/PrimefacesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.softwareValelciano.primefaces.primefacesDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimefacesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimefacesDemoApplication.class, args);
+	}
+
+}
